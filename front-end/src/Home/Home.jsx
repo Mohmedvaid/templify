efb69a156bbbb055be8cb6576814a2e4025785f2
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-09-28 21:12:35 -0500

    added banner

---
+++ front-end/src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <img className="home__image"
+            src="https://images-na.ssl-images-amazon.com/images/G/01/Events/2020/APDHF2/XCM_CUTTLE_1255085_1315904_US_PD20_3296237_1500x600_2X_US_en._CB402659267_.jpg" alt=""/>
+        </div>
+    )
+}
+
+export default Home
